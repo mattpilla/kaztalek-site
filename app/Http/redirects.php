dc759483f31b6101c650ca8121e68a425d0bc3a4
mattpilla <kaztalek@gmail.com>
2015-12-13 20:06:47 -0500

    adding files

---
+++ app/Http/redirects.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/2pause', function() {
+   return redirect('https://www.youtube.com/watch?v=KaDflIPG3f4');
+});
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('content')
+    <div id="404-container" class="text-center">
+        <h1>404</h1>
+        <img src="/images/errors/404.jpg"/>
+        <br>
+        <a href="/">Home</a>
+    </div>
+@endsection
