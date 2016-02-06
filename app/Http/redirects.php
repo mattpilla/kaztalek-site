9f09cddc3d088029e0b04ec1cb9247c3b287456d
mattpilla <kaztalek@gmail.com>
2016-02-06 16:35:01 -0500

    git still sucks

--- app/Http/redirects.php
+++ app/Http/redirects.php
@@ -1,5 +1,13 @@
 <?php
 
 Route::get('/2pause', function() {
-   return redirect('https://www.youtube.com/watch?v=KaDflIPG3f4');
+    return redirect('https://www.youtube.com/watch?v=KaDflIPG3f4');
+});
+
+Route::get('/ss-dolphin', function() {
+    return redirect('/downloads/venick/Skyward%20Sword%20Dolphin.7z');
+});
+
+Route::get('/kirby-where-ride', function() {
+    return redirect('/downloads/keeta/kirby%20hide%20and%20seek.7z');
 });
