8911491f5cb1b8cecb3ffafb1402e50e423c1c0b
mattpilla <kaztalek@gmail.com>
2016-04-13 21:20:46 -0400

    javascript fuck

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,28 +6,26 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>@yield('title')</title>
-        <link type="text/css" rel="stylesheet" href="/css/app.css" media="screen,projection" />
+        <link rel="stylesheet" href="/css/app.css" media="screen,projection" />
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
         @yield('styles')
     </head>
 
     <body>
-    <header>
-        {{-- @include('nav') --}}
-    </header>
-    <div class="container-fluid">
-        @yield('content')
-    </div>
-
-    <script type="text/javascript" src="/js/all.js"></script>
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
-    <script src='/js/app/master.js'></script>
-    @yield('scripts')
+        <header>
+            {{-- @include('nav') --}}
+        </header>
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+        <script src="/js/all.js"></script>
+        <script>
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        </script>
+        @yield('scripts')
     </body>
 </html>
