9c7a21da296fdc39f8f66e627c1c646726caf962
mattpilla <kaztalek@gmail.com>
2016-05-05 21:33:27 -0400

    City Trial events

--- resources/assets/js/app/kirby/city-trial.js
+++ resources/assets/js/app/kirby/city-trial.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-	$('.checkbox input').change(function(){
-		var c = this.checked ? '#555' : '#c7c7c7'
-	    $(this).parent().css('color', c);
-	});
+    $('.checkbox input[type="checkbox"]').change(function() {
+        var c = this.checked ? '#555' : '#c7c7c7'
+        $(this).parent().css('color', c);
+    });
 
-	$('.visibility').on('click', function() {
-		if ($(this).hasClass('plus')) {
-			$(this).addClass('btn-accent-ghost');
-			$(this).removeClass('plus btn-light-ghost');
-			$(this).html('-');
-			$('.' + $(this).attr('id')).show();
-		} else {
-			$(this).addClass('plus btn-light-ghost');
-			$(this).removeClass('btn-accent-ghost');
-			$(this).html('+');
-			$('.' + $(this).attr('id')).hide();
-		}
-		$(this).blur();
-	});
+    $('.checkbox input[type="number"]').change(function() {
+        if ($(this).val() >= $(this).attr('max')) {
+            $(this).prev().prop('checked', true).parent().css('color', '#555');
+        }
+    });
+
+    $('.visibility').on('click', function() {
+        if ($(this).hasClass('plus')) {
+            $(this).addClass('btn-accent-ghost');
+            $(this).removeClass('plus btn-light-ghost');
+            $(this).html('-');
+            $('.' + $(this).attr('id')).show();
+        } else {
+            $(this).addClass('plus btn-light-ghost');
+            $(this).removeClass('btn-accent-ghost');
+            $(this).html('+');
+            $('.' + $(this).attr('id')).hide();
+        }
+        $(this).blur();
+    });
 });
