0d43e83ddfc8a546da12f7baca76c6bb11accda0
mattpilla <kaztalek@gmail.com>
2016-05-10 21:26:41 -0400

    fixed up scenes page

--- resources/views/app/mm/scenes.blade.php
+++ resources/views/app/mm/scenes.blade.php
@@ -1,8 +1,18 @@
 @extends('master')
 
-@section('title', 'Scene ID')
+@section('title', 'Scene IDs')
 
 @section('content')
-<h1>Scene ID</h1>
-<iframe class="google-doc" src="https://docs.google.com/spreadsheets/d/165jXqfz7kpRAWVSsIth8iOxKZYaow8vlLFPCaZbXR24/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"></iframe>
+<div class="row">
+	<div class="col-sm-6">
+		<iframe class="google-doc pull-right" src="https://docs.google.com/spreadsheets/d/165jXqfz7kpRAWVSsIth8iOxKZYaow8vlLFPCaZbXR24/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false"></iframe>
+	</div>
+	<div class="col-sm-6 hidden-xs">
+		<h2>Majora's Mask Scene IDs</h2>
+		<p>
+			These are a set of IDs given to scenes internally. In game, Epona's stored scene ID <a href="https://www.youtube.com/watch?v=9UfbcxiUT3c">can be modified</a>.<br>
+			The value of that byte corresponds to this chart.
+		</p>
+	</div>
+</div>
 @endsection
