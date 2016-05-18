82923bf7be10d279daabc0a4de5f1a9a0ca42ae5
mattpilla <kaztalek@gmail.com>
2016-05-17 22:44:50 -0400

    mostly color shit

--- resources/views/app/games/cafdehd.blade.php
+++ resources/views/app/games/cafdehd.blade.php
@@ -3,36 +3,36 @@
 @section('title', 'Cafde\'s Quest for Satisfaction 1.5 HD REMIX')
 
 @section('content')
-	<div class="row">
-	    <div id="cafdehd-banner" class="col-xs-12">
-	        <h2>Cafde's Quest for Satisfaction 1.5 HD REMIX</h2>
-	    </div>
-	</div>
-	<div class="row">
-		<div class="col-lg-6 game-info">
-			<img src="/images/games/cafdehd/screen3.png" class="pull-left" />
-			<a href="/games/cafdehd/download" class="btn btn-lg btn-custom-primary pull-right">Download<span class="hd">HD</span></a>
-			<h3 class="primary-light">Cafde's Quest for Satisfaction 1.5 HD REMIX</h3>
-    		<p>
-    			Dunky dunky, what's in the trunky? Not satisfaction, that's for sure! <strong>Cafde's Quest</strong><span class="hd">HD</span> answers the burning question: <em>When life gives you sugar cubes, what do you do?</em> The answer? That's up to player interpretation. Regardless, help Cafde<span class="hd">HD</span> grab his trusty <span id="special-text">ANIME PRO</span> mug and keep his coffee untainted for as long as possible. That's right, we put your brand in their hand! There is no winning in this cruel world.
-    		</p>
-    		<p>
-    			Move around with the arrow keys and levitate with the space bar.
-    		</p>
-    		<p>
-    			This game was made in high definition.
-			</p>
-			<div class="requirements">
-				<h4 class="primary-light">Requirements:</h4>
-				<span class="require">OS:</span> Windows<span class="hd">HD</span><br>
-				<span class="require">Dependencies:</span> 4k monitor<br>
-				<span class="require">Space:</span> 26MB
-			</div>
-		</div>
-		<div class="col-lg-6">
-			<div class="screen-holder">
-				<img id="cafdehd-end" src="/images/games/cafdehd/screen2.png" />
-			</div>
-		</div>
-	</div>
+    <div class="row">
+        <div id="cafdehd-banner" class="col-xs-12">
+            <h2>Cafde's Quest for Satisfaction 1.5 HD REMIX</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 game-info">
+            <img src="/images/games/cafdehd/screen3.png" class="pull-left" />
+            <a href="/games/cafdehd/download" class="btn btn-lg btn-primary-ghost pull-right">Download<span class="hd">HD</span></a>
+            <h3>Cafde's Quest for Satisfaction 1.5 HD REMIX</h3>
+            <p>
+                Dunky dunky, what's in the trunky? Not satisfaction, that's for sure! <strong>Cafde's Quest</strong><span class="hd">HD</span> answers the burning question: <em>When life gives you sugar cubes, what do you do?</em> The answer? That's up to player interpretation. Regardless, help Cafde<span class="hd">HD</span> grab his trusty <span id="special-text">ANIME PRO</span> mug and keep his coffee untainted for as long as possible. That's right, we put your brand in their hand! There is no winning in this cruel world.
+            </p>
+            <p>
+                Move around with the arrow keys and levitate with the space bar.
+            </p>
+            <p>
+                This game was made in high definition.
+            </p>
+            <div class="requirements">
+                <h4>Requirements:</h4>
+                <span class="require">OS:</span> Windows<span class="hd">HD</span><br>
+                <span class="require">Dependencies:</span> 4k monitor<br>
+                <span class="require">Space:</span> 26MB
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="screen-holder">
+                <img id="cafdehd-end" src="/images/games/cafdehd/screen2.png" />
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/app/games/dungeon.blade.php
+++ resources/views/app/games/dungeon.blade.php
@@ -11,8 +11,8 @@
     <div class="row">
         <div class="col-lg-6 game-info">
             <img src="/images/games/dungeon/git2.png" class="pull-left" />
-            <a href="/games/dungeon/download" class="btn btn-lg btn-custom-primary pull-right">Download</a>
-            <h3 class="primary-light">Dungeon</h3>
+            <a href="/games/dungeon/download" class="btn btn-lg btn-primary-ghost pull-right">Download</a>
+            <h3>Dungeon</h3>
             <p>
                 As the name suggests, <strong>Dungeon</strong> is a game that revolves around escaping a small dungeon. Unlike what you may expect, there are no enemies here. Rather, you'll find the act of escaping to be a puzzling matter. There's 2 items hidden throughout the dungeon. Use them to help complete your quest! You may find yourself visiting each room more than once...
             </p>
@@ -23,7 +23,7 @@
                 This Zelda-inspired project was made in a month, and finished in December 2012. The game is short; a first playthrough may take around 20 to 30 minutes.
             </p>
             <div class="requirements">
-                <h4 class="primary-light">Requirements:</h4>
+                <h4>Requirements:</h4>
                 <span class="require">OS:</span> Windows <em>(Probably no older than XP)</em><br>
                 <span class="require">Dependencies:</span> Java <em>(Sorry)</em><br>
                 <span class="require">Space:</span> 6MB <em>(If you're on this page, you have enough room)</em>
