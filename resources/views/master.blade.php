7f9f1dfc09e3187235ccc1891eb881fa0847bd7a
mattpilla <kaztalek@gmail.com>
2017-03-28 21:35:33 -0400

    meta shit

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,4 +1,29 @@
 <!DOCTYPE html>
+<!--
+      MMMMMMM          MMM
+    MM:      MM       MZ77M
+   M+          M      M777ZM
+  M+           :M    M777$8M
+  M:            :M  M77ZZ8M
+ M+            M M M77ZZMM
+ M+      MM    M :M77Z$M
+ M:      MM     MM77ZMM
+ M:           MM$77ZM
+M+:          MZ77ZOM
+M::          M77ZMMM  fuck
+M::          M777OMM
+M::           MZ77OM
+M::            M$77$MM
+M+:             MZ777ZMM
+ M:              MZZ777ZM
+ M::              M8Z7777M
+ M+:              MMMZ$77M
+  M:            ::M  M8ZZM
+  M++::        ::M    MMM
+   M+::::::::::+M
+    MM++::::++MM
+      MMMMMMMM
+-->
 <html lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -6,6 +31,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>@yield('title')</title>
+
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="kaztalek.com">
+        <meta name="twitter:description" content="the shittiest website on this earth">
+        <meta name="twitter:image" content="https://kaztalek.com/public/images/preview120.png">
+
         <link rel="stylesheet" href="/css/app.css" media="screen,projection" />
         @yield('styles')
         <script defer src="/js/all.js"></script>
