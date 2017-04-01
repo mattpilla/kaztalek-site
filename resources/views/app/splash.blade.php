ab6e124e695042c7268209a8887145d8472cc135
mattpilla <kaztalek@gmail.com>
2017-03-31 23:00:12 -0400

    updated home page

--- resources/views/app/splash.blade.php
+++ resources/views/app/splash.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'Kaztalek')
 
 @section('content')
-    nothing to see here (ever)<br>
+    hey<br>
+    this site is a piece of shit. used specifically for personal resources<br>
+    there will never be anything interesting here. enjoy<br>
     <script src="https://en.lichess.org/tv/embed?theme=green&bg=dark"></script>
 @endsection
